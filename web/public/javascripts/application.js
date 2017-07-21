21dc8a8603a5374220ba306bdefb60f2fd5d0396
viktor <baranov.viktor.27@gmail.com>
2017-07-21 16:43:28 +0300

    swal fix

--- web/public/javascripts/application.js
+++ web/public/javascripts/application.js
@@ -45,7 +45,7 @@ function showAlert(err, msg, type) {
 	if (err.type == "REQUEST_REJECTED") return;
 	swal({
 	  title: type?capitalizeFirstLetter(type):"Error",
-	  text: msg?msg:err.message,
+	  html: msg?msg:err.message,
 	  type: type?type:"error"
 	});
 }
